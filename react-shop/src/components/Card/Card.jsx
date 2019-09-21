ed707a2920ae6e309232e6f4fd0e8b088041bb94
leonidshparun <leonidshparun@gmail.com>
2019-09-21 16:58:13 +0300

    feat: add tips for cards

--- react-shop/src/components/Card/Card.jsx
+++ react-shop/src/components/Card/Card.jsx
@@ -19,7 +19,9 @@ const CardContainer = styled.div`
 	align-items: center;
 	user-select: none;
 	transition: border-color 0.2s;
-
+	position: relative;
+	background-color: ${props => props.show ? 'honeydew' : ''};
+	
 	:hover {
 		border: thin solid #009288;
 	}
@@ -39,8 +41,31 @@ const Price = styled.p`
 	color: #0b5a53;
 `;
 
+const Tip = styled.div`
+	background-color: #009688d1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	height: calc(100% - 6px);
+	width: calc(100% - 6px);    
+	visibility: ${props => props.show ? 'visible' : 'hidden'};
+
+	p {
+		width: 100%;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 600;
+    color: white;
+    background: #FFC107;
+    padding: 10px;
+	}
+`;
+
 class Card extends Component {
 	state = {
+		tipText: 'Select size',
+		showTip: false,
 		selectedSize: [],
 	}
 
@@ -50,17 +75,22 @@ class Card extends Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault();
-		if (!this.state.selectedSize[0]) alert(`Choose size, please`);
-		else {
-			this.props.add(
-				{ id: this.props.data.id, size: this.state.selectedSize[0] }
-			)
+		if (!this.state.selectedSize[0]) {
+			this.setState({ showTip: true });
+			setTimeout(() => this.setState({ showTip: false, tipText: 'Select size' }), 900);
+		} else {
+			this.props.add({ id: this.props.data.id, size: this.state.selectedSize[0] });
+			this.setState({ showTip: true, tipText: 'Added' });
+			setTimeout(() => this.setState({ showTip: false }), 500);
 		};
 	}
 
 	render() {
 		return (
-			<CardContainer>
+			<CardContainer show={this.state.showTip}>
+				<Tip show={this.state.showTip}>
+					<p>{this.state.tipText}</p>
+				</Tip>
 				<img
 					src={require(`../../static/img/item${this.props.data.id}.jpg`)}
 					alt={this.props.data.title}
