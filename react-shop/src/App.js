12282927d4c88aebbaf52cb95031fbd60ac2846d
leonidshparun <leonidshparun@gmail.com>
2019-09-21 16:57:03 +0300

    feat: implement modal window

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
 
-const App = () => {
-  return (
-    <Products />
-  )
+import styled from 'styled-components';
+
+import Modal from './components/UI/Modal/Modal';
+
+const AppContainer = styled.main`
+  position: relative;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <AppContainer>
+        <Products />
+        <Cart />
+        <Modal />
+      </AppContainer >
+    )
+  }
 }
 
 export default App;
---
+++ react-shop/src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styled from 'styled-components';
+
+const modalRoot = document.getElementById('modal-root');
+
+const ModalContainer = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalContent = styled.div`
+  background-color: #009688f0;
+	height: 150px;
+	width: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+class Modal extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showModal: true,
+			content: 'MODAL CONTENT',
+		};
+		this.el = document.createElement('div');
+	}
+
+
+	handleShow = () => {
+		this.setState({ showModal: true });
+	}
+
+	handleHide = () => {
+		this.setState({ showModal: false });
+	}
+
+	setTimer = (time) => {
+		if (time) setTimeout(() => this.setState({ showModal: false }), time)
+	}
+
+
+	componentDidMount() {
+		modalRoot.appendChild(this.el);
+		this.setTimer(1000);
+	}
+
+	componentWillUnmount() {
+		modalRoot.removeChild(this.el);
+	}
+
+	render() {
+		const modal = this.state.showModal ?
+			<ModalContainer onClick={this.handleHide}>
+				<ModalContent>
+					{this.state.content}
+				</ModalContent>
+			</ModalContainer> :
+			'';
+		return ReactDOM.createPortal(
+			modal,
+			this.el,
+		);
+	}
+}
+
+export default Modal;
