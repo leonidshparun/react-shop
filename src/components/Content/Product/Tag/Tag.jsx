8ba6851ef3e46586b18739588a8a50c10ebd3ea2
leonidshparun <leonidshparun@gmail.com>
2019-10-08 21:44:37 +0300

    refactor: change folders structure

--- src/components/Content/Product/Tag/Tag.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Container from './style';
-
-const Tag = ({ discount }) =>
-  discount !== 0 ? <Container>{`${-discount}%`}</Container> : null;
-
-export default Tag;
